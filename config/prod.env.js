845d1a320358481dc470e111f9373a877e051447
LZhao <hljtlzyx@live.ocm>
2021-06-11 12:59:30 +0800

    modify production api root

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT:'"https://lzo-web-app.azurewebsites.net/back-end-0.0.1-SNAPSHOT"'
+  API_ROOT:'"https://xxxxxxxxxxxx/back-end-0.0.1-SNAPSHOT"'
 }
